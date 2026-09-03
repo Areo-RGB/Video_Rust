aab127e803a5416660173d5244cd58f97d20f92a
Areo-RGB <paziske@gmail.com>
2026-09-03 08:20:16 +0200

    app: use hardcoded remote data source

--- src/app.rs
+++ src/app.rs
@@ -3,10 +3,9 @@ use std::sync::mpsc::{Receiver, Sender, channel};
 
 use eframe::egui;
 
-use crate::config::AppConfig;
+use crate::config::{AppConfig, DATA_JSON_RAW_URL, DATA_JSON_SOURCE_URL};
 use crate::data_json::{
-    add_chapter, add_playlist, load_data, remove_chapter, remove_playlist, save_data,
-    upsert_bundle_playlist,
+    add_chapter, add_playlist, fetch_data, remove_chapter, remove_playlist, upsert_bundle_playlist,
 };
 use crate::jobs::{JobMessage, JobValue, spawn_job};
 use crate::local::{
@@ -84,8 +83,7 @@ pub struct VideoManagerApp {
 impl Default for VideoManagerApp {
     fn default() -> Self {
         let config = AppConfig::load();
-        let data_path = config.data_json_path.clone();
-        let data = load_data(Path::new(&data_path)).unwrap_or_default();
+        let data_path = DATA_JSON_SOURCE_URL.to_owned();
         let bundles = scan_bundles(Path::new(&config.workspace_dir)).unwrap_or_default();
         let (job_tx, job_rx) = channel();
         let yt_version = tool_version(&config.yt_dlp_path, "--version");
@@ -99,7 +97,7 @@ impl Default for VideoManagerApp {
             selected_bundle: None,
             local_file_filter: String::new(),
             r2_objects: Vec::new(),
-            data,
+            data: DataFile::default(),
             data_path,
             selected_playlist: None,
             new_playlist: String::new(),
